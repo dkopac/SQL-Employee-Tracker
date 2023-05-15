184079e2878d2f74daf1061160c81b490602d60d
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-15 18:16:00 -0400

    updated seeds

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -5,12 +5,12 @@ VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Lead', 100000.00, 1),
-       ('Salesperson', 80000.00, 1),
-       ('Lead Engineer', 150000.00, 2),
-       ('Software Engineer', 120000.00, 2),
-       ('Accountant', 125000.00, 3),
-       ('Legal Team Lead', 250000.00, 4),
-       ('Lawyer', 190000.00, 4);
+       ('Salesperson', 80000.00, 2),
+       ('Lead Engineer', 150000.00, 3),
+       ('Software Engineer', 120000.00, 4),
+       ('Accountant', 125000.00, 5),
+       ('Legal Team Lead', 250000.00, 6),
+       ('Lawyer', 190000.00, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL), --Sales Lead
@@ -18,5 +18,5 @@ VALUES ('John', 'Doe', 1, NULL), --Sales Lead
        ('Ashley', 'Rodriguez', 3, NULL), --Lead Engineer
        ('Kevin', 'Tupik', 4, 3), --Software Engineer
        ('Malia', 'Brown', 5, NULL), --Accountant
-       ('Sarah', 'Lourd', 6, 5), --Legal Team Lead
-       ('Tom', 'Allen', 7, NULL); --Lawyer
+       ('Sarah', 'Lourd', 6, NULL), --Legal Team Lead
+       ('Tom', 'Allen', 7, 6); --Lawyer
